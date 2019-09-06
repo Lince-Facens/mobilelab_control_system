9c3b111a1cb93e2f755a14c5c08295da2a1cb65a
ehs <emanuel.tesv@gmail.com>
2019-09-06 20:08:14 -0300

    Add ADC configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,210 @@
-/*
- * This file is part of the µOS++ distribution.
- *   (https://github.com/micro-os-plus)
- * Copyright (c) 2014 Liviu Ionescu.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom
- * the Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+/**
+ ******************************************************************************
+ * @file    mobilelab_data_gathering/main.cpp
+ * @author  Emanuel Huber da Silva
+ * @version V1.0
+ * @date    06-August-2019
+ * @brief   Main program body
+ ******************************************************************************
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define ADC1_DR    ((uint32_t)0x4001244C)
+#define ARRAYSIZE 3
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+ADC_InitTypeDef ADC_InitStructure;
+DMA_InitTypeDef DMA_InitStructure;
+uint32_t status;
+__IO uint16_t ADC_values[ARRAYSIZE];
+
+/* Private function prototypes -----------------------------------------------*/
+void RCC_Configuration(void);
+void GPIO_Configuration(void);
+void NVIC_Configuration(void);
+void DMA_Configuration(void);
+void ADC_Configuration(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+ * @brief   Main program
+ * @param  None
+ * @retval None
  */
-
-// ----------------------------------------------------------------------------
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "diag/Trace.h"
-
-// ----------------------------------------------------------------------------
-//
-// Standalone STM32F1 empty sample (trace via NONE).
-//
-// Trace support is enabled by adding the TRACE macro definition.
-// By default the trace messages are forwarded to the NONE output,
-// but can be rerouted to any device or completely suppressed, by
-// changing the definitions required in system/src/diag/trace_impl.c
-// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
-//
-
-// ----- main() ---------------------------------------------------------------
-
-// Sample pragmas to cope with warnings. Please note the related line at
-// the end of this function, used to pop the compiler diagnostics status.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-#pragma GCC diagnostic ignored "-Wreturn-type"
-
-int
-main(int argc, char* argv[])
+int main(void)
 {
-  // At this stage the system clock should have already been configured
-  // at high speed.
+	/*!< At this stage the microcontroller clock setting is already configured,
+	 this is done through SystemInit() function which is called from startup
+	 file (startup_stm32f10x_xx.s) before to branch to application main.
+	 To reconfigure the default setting of SystemInit() function, refer to
+	 system_stm32f10x.c file
+	 */
 
-  // Infinite loop
-  while (1)
-    {
-       // Add your code here.
-    }
+	/* System clocks configuration ---------------------------------------------*/
+	RCC_Configuration();
+
+	/* GPIO configuration ------------------------------------------------------*/
+	GPIO_Configuration();
+
+	/* DMA configuration ------------------------------------------------------*/
+	DMA_Configuration();
+
+	/* NVIC configuration ------------------------------------------------------*/
+	NVIC_Configuration();
+
+	/* ADC configuration ------------------------------------------------------*/
+	ADC_Configuration();
+
+	while (1)
+	{
+	}
 }
 
-#pragma GCC diagnostic pop
+/**
+ * @brief  Configures the ADC.
+ * @param  None
+ * @retval None
+ */
+void ADC_Configuration(void)
+{
+	/* ADC1 configuration ------------------------------------------------------*/
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfChannel = 3;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	/* ADC1 regular channels configuration */
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_28Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 2, ADC_SampleTime_28Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 3, ADC_SampleTime_28Cycles5);
+	/* Enable ADC1 DMA */
+	ADC_DMACmd(ADC1, ENABLE);
 
-// ----------------------------------------------------------------------------
+	/* Enable ADC1 */
+	ADC_Cmd(ADC1, ENABLE);
+
+	/* Enable ADC1 reset calibration register */
+	ADC_ResetCalibration(ADC1);
+	/* Check the end of ADC1 reset calibration register */
+	while (ADC_GetResetCalibrationStatus(ADC1));
+
+	/* Start ADC1 calibration */
+	ADC_StartCalibration(ADC1);
+	/* Check the end of ADC1 calibration */
+	while (ADC_GetCalibrationStatus(ADC1))
+		;
+
+	//Start ADC1 Software Conversion
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+	//wait for DMA complete
+	while (!status) {};
+	ADC_SoftwareStartConvCmd(ADC1, DISABLE);
+}
+
+/**
+ * @brief  Configures the DMA for reading the ADC values.
+ * @param  None
+ * @retval None
+ */
+void DMA_Configuration(void)
+{
+	/* DMA1 channel1 configuration ----------------------------------------------*/
+	DMA_DeInit(DMA1_Channel1);
+	DMA_InitStructure.DMA_BufferSize = ARRAYSIZE;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t) ADC1_DR;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t) ADC_values;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+	/* Enable DMA1 channel1 */
+	DMA_ITConfig(DMA1_Channel1, DMA_IT_TC, ENABLE);
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+
+	//Enable DMA1 Channel transfer
+	DMA_Cmd(DMA1_Channel1, ENABLE);
+}
+
+/**
+ * @brief  Configures the different system clocks.
+ * @param  None
+ * @retval None
+ */
+void RCC_Configuration(void)
+{
+	/* ADCCLK = PCLK2/4 */
+	RCC_ADCCLKConfig(RCC_PCLK2_Div4);
+
+	/* Enable peripheral clocks ------------------------------------------------*/
+	/* Enable DMA1 and DMA2 clocks */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+	/* Enable ADC1, ADC2, ADC3 and GPIOC clocks */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA, ENABLE);
+}
+
+/**
+ * @brief  Configures the different GPIO ports.
+ * @param  None
+ * @retval None
+ */
+void GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Configure PC.02, PC.03 and PC.04 (ADC Channel12, ADC Channel13 and
+	 ADC Channel14) as analog inputs */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+/**
+ * @brief  Configures Vector Table base location.
+ * @param  None
+ * @retval None
+ */
+void NVIC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+	//Enable DMA1 channel IRQ Channel */
+	NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+	{
+	}
+}
+
+#endif
